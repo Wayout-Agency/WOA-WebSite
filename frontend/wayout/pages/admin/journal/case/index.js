13c0ac793d9ee0c3e484906f77aa5735e5c20856
BazaroZero <finatov05@gmail.com>
2022-08-06 15:29:33 +0300

    fix(frontend): rename component when importing

--- frontend/wayout/pages/admin/journal/case/index.js
+++ frontend/wayout/pages/admin/journal/case/index.js
@@ -1,9 +1,9 @@
 import AdminLayout from "@/components/AdminPanel/AdminLayout";
-import Case from "@/components/AdminPanel/Journal/CreateCase";
+import CreateCase from "@/components/AdminPanel/Journal/CreateCase";
 export default function Case() {
   return (
     <AdminLayout>
-      <Case />
+      <CreateCase />
     </AdminLayout>
   );
 }
