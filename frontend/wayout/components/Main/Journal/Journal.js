4f566b88c4fe190ebe4b6ea3318c316b5a371911
Flict-dev <zhdanom.maksi@gmail.com>
2022-08-12 23:09:37 +0300

    WIP: feat(frontend): add main page
    Add journal component to main page

---
+++ frontend/wayout/components/Main/Journal/Journal.js
@@ -0,0 +1,105 @@
+import styles from "./Journal.module.scss";
+import wayoutAPI from "services/wayoutApi";
+import useSWR from "swr";
+import Link from "next/link";
+import Image from "next/image";
+import { default as ImageRaw } from "next/future/image";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import config from "config";
+import clientsStyles from "../../UI/Clients/Clients.module.scss";
+const Journal = () => {
+  const articlesApiUrl = "/posts/articles/?quantity=3";
+
+  const fetcher = async () => {
+    const response = await wayoutAPI.get(articlesApiUrl);
+    console.log(response.data);
+    return response.data;
+  };
+  const { data, error } = useSWR(articlesApiUrl, fetcher);
+
+  if (error) throw "Error";
+
+  return (
+    <div className={styles.journalWrapper}>
+      <h2 className={styles.title}>Журнал</h2>
+      {data && data.length > 2 ? (
+        <div className={styles.articlesWrapper}>
+          <Link href={`/journal/articles/${data[0].value.slug}`}>
+            <div className={styles.bigImgWrapper}>
+              <div
+                className={styles.imgWrapper}
+                style={{ width: 750, height: 500 }}
+              >
+                <Image
+                  layout="fill"
+                  src={`${config.apiUrl}/files/articles/${data[0].value.id}/0/`}
+                  className={styles.img}
+                />
+              </div>
+              <h3 className={styles.postTitile}>
+                {data[0].value.title}
+                <br />
+                <span className={styles.span}>
+                  {data[0].value.created_at.slice(8) +
+                    "." +
+                    data[0].value.created_at.slice(5, 7) +
+                    ".22"}
+                </span>
+              </h3>
+            </div>
+          </Link>
+          <div className={styles.smallImgWrapper}>
+            {data.slice(1, 3).map(({ value }) => {
+              return (
+                <Link href={`/journal/articles/${value.slug}`}>
+                  <div className={styles.smallImgItemWrapper}>
+                    <div
+                      className={styles.imgWrapper}
+                      style={{ width: 183, height: 164 }}
+                    >
+                      <Image
+                        layout="fill"
+                        src={`${config.apiUrl}/files/articles/${value.id}/0/`}
+                        className={styles.img}
+                      />
+                    </div>
+                    <h3 className={styles.smallImgTitle}>
+                      {value.title}
+                      <br />
+                      <span className={styles.span}>
+                        {value.created_at.slice(8) +
+                          "." +
+                          value.created_at.slice(5, 7) +
+                          ".22"}
+                      </span>
+                    </h3>
+                  </div>
+                </Link>
+              );
+            })}
+            <Link href={"/journal/"}>
+              <div
+                className={clientsStyles.moreWrapper}
+                style={{ marginTop: 100 }}
+              >
+                <p className={clientsStyles.moreText}>Смотреть больше</p>
+                <ImageRaw
+                  className={clientsStyles.moreImg}
+                  width={66}
+                  height={28}
+                  src={"/static/img/arrow.svg"}
+                  layout="raw"
+                />
+              </div>
+            </Link>
+          </div>
+        </div>
+      ) : (
+        <Skeleton height={600} borderRadius={15} width={"100%"} />
+      )}
+    </div>
+  );
+};
+
+export default Journal;
