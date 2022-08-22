45eff8816617a662bc3340d566c71819a9b0f970
Flict-dev <zhdanom.maksi@gmail.com>
2022-08-22 14:10:33 +0300

    feat(frontend): add adaptive layout
    For burger menu
    Sizes from 480px to 800px

--- frontend/wayout/components/UI/BurgerMenu/BurgerMenu.js
+++ frontend/wayout/components/UI/BurgerMenu/BurgerMenu.js
@@ -22,7 +22,7 @@ const BurgerMenu = ({ click, setClick }) => {
               >
                 {title}
               </a>
-              <ExpImage width={46} height={20} src={"/static/img/arrow.svg"} />
+              <ExpImage width={46} height={20} src={"/static/img/arrow.svg"} className={styles.arrow}/>
             </div>
           </Link>
         ))}
