7b125750a0bbf9ff5bfefb37ef1aff3fc2b0bc6c
Flict-dev <zhdanom.maksi@gmail.com>
2022-09-01 22:10:12 +0300

    fix(frontend): remove a random space

--- frontend/wayout/components/Global/Footer/SocialMedias/SocialLinks.js
+++ frontend/wayout/components/Global/Footer/SocialMedias/SocialLinks.js
@@ -1,6 +1,6 @@
 const socialLinks = [
   { id: 1, title: "Telegram", path: "https://t.me/+_KsvdkvBDARhOWI6" },
-  { id: 2, title: "What's App", path: " https://wa.me/79858604455" },
+  { id: 2, title: "What's App", path: "https://wa.me/79858604455" },
   { id: 3, title: "TikTok", path: "https://www.tiktok.com/@wayout.albums" },
   { id: 4, title: "Instagram", path: "https://www.instagram.com/wayout.albums/" },
 ];
