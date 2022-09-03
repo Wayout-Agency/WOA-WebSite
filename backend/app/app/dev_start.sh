071b6ebe99880409c0831b895b6752c7932f0dc2
Flict-dev <zhdanom.maksi@gmail.com>
2022-09-03 11:01:22 +0300

    fix(dev_start): remove random character

--- backend/app/app/dev_start.sh
+++ backend/app/app/dev_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --name dev-pg -p 5432:5432 -e POSTGRES_PASSWORD=wayout_dev -e POSTGRES_USER=wayout_dev -e POSTGRES_PASSWORD=wayout_dev -d postgres:14.4Я
+docker run --name dev-pg -p 5432:5432 -e POSTGRES_PASSWORD=wayout_dev -e POSTGRES_USER=wayout_dev -e POSTGRES_PASSWORD=wayout_dev -d postgres:14.4
